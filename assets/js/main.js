9a920091d6eb9e638c4e3ace362a0ac83ec656cf
Iván Flores <hivansan@gmail.com>
2021-07-22 15:21:38 -0500

    pretty code

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,24 +1,24 @@
 var imgArray = [
-    "assets/images/yfu.png",
-    "assets/images/yfu2.png",
-    "assets/images/yfu3.png",
-    "assets/images/yfu4.png",
-    "assets/images/yfu5.png",
-    "assets/images/yfu6.png",
+  'assets/images/yfu.png',
+  'assets/images/yfu2.png',
+  'assets/images/yfu3.png',
+  'assets/images/yfu4.png',
+  'assets/images/yfu5.png',
+  'assets/images/yfu6.png',
 ];
 
 var curIndex = 0;
 var waifu = document.getElementById('yfu');
 
 function timeout() {
-    if (curIndex >= 0 && curIndex <= 5) {
-        waifu.src = imgArray[curIndex];
-        curIndex++;
-    } else {
-        curIndex = 0;
-    }
+  if (curIndex >= 0 && curIndex <= 5) {
+    waifu.src = imgArray[curIndex];
+    curIndex++;
+  } else {
+    curIndex = 0;
+  }
 
-    setTimeout(timeout, 5000);
+  setTimeout(timeout, 5000);
 }
 
 timeout();
